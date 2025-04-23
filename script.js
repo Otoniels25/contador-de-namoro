3fce54aaf7a788e586c9dd489af08fe2399463de
Otoniels25 <otoniellsouza@gmail.com>
2025-04-22 21:35:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 
 function calcularDias() {
-  const inicioNamoro = new Date("2025-04-06");
-  const inicioOracao = new Date("2025-03-10");
+  const inicio = new Date("2025-04-06");
   const hoje = new Date();
-
-  const diasNamoro = Math.floor((hoje - inicioNamoro) / (1000 * 60 * 60 * 24));
-  const diasOracao = Math.floor((hoje - inicioOracao) / (1000 * 60 * 60 * 24));
-
-  document.getElementById("contador-namoro").innerText = `${diasNamoro} dias de namoro`;
-  document.getElementById("contador-oracao").innerText = `${diasOracao} dias no propósito de oração`;
+  const diferenca = hoje - inicio;
+  const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+  document.getElementById("contador").innerText = `${dias} dias`;
 }
 
 calcularDias();
